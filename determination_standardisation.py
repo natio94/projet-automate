151724c2ba89453e42d9e977c19c0e1a6940e018
StonksProp <151038048+StonksProp@users.noreply.github.com>
2025-03-20 12:20:14 +0100

    .

--- determination_standardisation.py
+++ determination_standardisation.py
@@ -1,3 +1,15 @@
-def standardisation(){
-    print("bite")
-}
+def est_standardisation(fichier):
+    i = 0
+    with open(fichier, 'r'):
+        for line in fichier:
+            if(i == 2 and line[0] != '1'):
+                return 0
+
+            u = int(line[2])
+
+            if(i >= 5):
+                if(line[2] == u):
+                    return 0
+
+            i += 1
+    return 1
